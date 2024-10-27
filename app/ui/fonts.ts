1c7c22af848accb0abff8b2fe24a8cea13e60aa5
q1anxiaodong <1465382456@qq.com>
2024-10-27 22:51:40 +0800

    chapter3: 优化字体和图片-练习代码

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const Lusitana = Inter({ 
+export const lusitana = Inter({ 
     subsets: ['latin'],
     weight: ['400', '700']
 })
